b8eef5a0e8c33866e793389203d25caed06a7621
Richard Levitte <levitte@openssl.org>
2023-12-07 18:24:05 +0100

    Demonstration of creating and cleaning up a provider context

--- helloworld.c
+++ helloworld.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include <openssl/core.h>
 #include <openssl/core_dispatch.h>
 
+struct provctx_st {
+    int dummy;
+};
+
+static OSSL_FUNC_provider_teardown_fn teardown;
+void teardown(void *provctx)
+{
+    free(provctx);
+}
+
 const OSSL_DISPATCH provfns[] = {
+    { OSSL_FUNC_PROVIDER_TEARDOWN, (void (*)(void))teardown },
     { 0, NULL }
 };
 
@@ -13,6 +26,11 @@ int OSSL_provider_init(const OSSL_CORE_HANDLE *handle,
                        void **provctx)
 {
     fprintf(stderr, "nonsense: Hello, world!\n");
+
+    struct provctx_st *pctx = malloc(sizeof(struct provctx_st));
+
+    memset(pctx, 0, sizeof(*pctx));
+    *provctx = pctx;
     *out = provfns;
     return 1;
 }
